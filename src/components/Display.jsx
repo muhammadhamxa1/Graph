a11ff134c6a35f0ef5b1ac32b660298b55a7252f
Muhammad Hamza <40244379+muhammadhamxa1@users.noreply.github.com>
2021-11-27 21:24:33 +0500

    hoop

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -27,7 +27,7 @@ const Display = () => {
   return (
     <>
       <Container id="con">
-        <h6 id="top">Hy Tahoor</h6>
+        <h6 id="top">hy heelo</h6>
         <Row>
           <Col xl="3" xs="12">
           <Dropdown>
